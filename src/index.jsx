a0603bbc58d62e62883992fecfcf78cfd823f0e2
jeffpsrb <jeffreyp454r1bu@gmail.com>
2023-08-22 16:39:32 +0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Validation from "./validation";
+// import FormElemen from "./pembahasan/formElemen";
+
+export default class Form extends React.Component {
+    render(){
+        return(
+            <Validation />
+        )
+    }
+}
